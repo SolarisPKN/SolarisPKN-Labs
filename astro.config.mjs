bcfca71093158b66bff581adee82ad3e2f1f0f40
SolarisPKN <pablokoutanya@gmail.com>
2026-06-16 03:00:10 -0300

    Add new pages and translations
    Add 404 and under construction pages
    Add language selector and translations
    Add favicon
    Add Portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://solarispkn.github.io',
+  site: 'https://solarispkn.pages.dev',
   base: '/',
   output: 'static',
   build: {
     assets: 'assets'
-  }
+  },
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 });
